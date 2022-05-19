658e67fa5528a3980a764afba9e58328ed095614
Terrel Shumway <skywalkereternal+carnd@gmail.com>
2022-05-19 05:48:15 -0600

    first compiled 'seniply' layers

--- keyboards/keyboardio/atreus/keymaps/lernisto-minimak/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/lernisto-minimak/keymap.c
@@ -13,66 +13,47 @@ I want a hybrid between OSL and TT.
 
 
 enum layer_names {
-	_ISRTNEAO, 
-    _MINIMAK,
-	_QWERTY, 
-	_COLEMAK, 
-    _NUM, // function keys & numbers
-	_NAV, // navigation & mouse
-	_SYM, // symbols
-    _KYB, // keyboard control
+	_BASE, 
+	_EXTEND,
+	_FUNCTION,
+    _SYMNUM, 
+	_MOUSE, 
+	//_KYB, // keyboard control
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_ISRTNEAO] = LAYOUT(
+	[_BASE] = LAYOUT(
 		         KC_Q,       KC_C,           KC_L,           KC_M,       KC_K,                                               KC_B,          KC_F,          KC_U,  KC_COMM,   KC_SCLN,
-		         KC_I,       KC_S,   LALT_T(KC_R),   LGUI_T(KC_T),       KC_G,                                               KC_P,  RGUI_T(KC_N),  RALT_T(KC_E),     KC_A,      KC_O, 
+		         KC_I,       KC_S,           KC_R,           KC_T,       KC_G,                                               KC_P,          KC_N,          KC_E,   KC_A,      KC_O, 
 		         KC_Y,       KC_V,           KC_W,           KC_D,       KC_X,          KC_Z,             KC_BSLS,           KC_J,          KC_H,       KC_MINS,   KC_DOT,   KC_QUOT, 
-      LCTL_T(KC_ESC),   TLL(_NUM),      TLL(_NAV),      TLL(_SYM), OSM(MOD_LSFT),    KC_BSPC,              KC_TAB,  MEH_T(KC_SPC),         KC_NO,       KC_SLSH,   KC_GRV,   RCTL_T(KC_ENT)
+       LCTL_T(KC_ESC),  TLL(_FUNCTION),     KC_NO,   TLL(_EXTEND), OSM(MOD_LSFT),    KC_BSPC,            KC_TAB,  MEH_T(KC_SPC),   TLL(_SYMNUM),       KC_SLSH,   KC_GRV,   RCTL_T(KC_ENT)
 		),
-	[_MINIMAK] = LAYOUT(
-		         KC_Q,       KC_W,           KC_D,           KC_F,       KC_K,                                               KC_Y,          KC_U,          KC_I,     KC_O,   KC_SCLN,
-		         KC_A,       KC_S,   LALT_T(KC_R),   LGUI_T(KC_T),       KC_G,                                               KC_H,  RGUI_T(KC_N),  RALT_T(KC_E),     KC_L,      KC_P, 
-		      KC_QUOT,       KC_X,           KC_C,           KC_V,       KC_B,          KC_Z,             KC_BSLS,           KC_J,          KC_M,       KC_COMM,   KC_DOT,   KC_SLSH, 
-    	      _______,     _______,        _______,        _______,    _______,       _______,             _______,        _______,       _______,       KC_MINS,  _______,   _______
-		),
-	[_QWERTY] = LAYOUT(
-		         KC_Q,       KC_W,           KC_E,           KC_R,       KC_T,                                               KC_Y,          KC_U,          KC_I,     KC_O,      KC_P,
-		         KC_A,       KC_S,   LALT_T(KC_D),   LGUI_T(KC_F),       KC_G,                                               KC_H,  RGUI_T(KC_J),  RALT_T(KC_K),     KC_L,   KC_SCLN, 
-			     KC_Z,       KC_X,           KC_C,           KC_V,       KC_B,         KC_QUOT,           KC_BSLS,           KC_N,          KC_M,       KC_COMM,   KC_DOT,   KC_SLSH, 
-    	     _______,     _______,        _______,        _______,    _______,       _______,             _______,        _______,       _______,       KC_MINS,  _______,   _______
-		),
-	[_COLEMAK] = LAYOUT(
-		         KC_Q,       KC_W,           KC_F,           KC_P,       KC_B,                                               KC_J,          KC_L,          KC_U,     KC_Y,   KC_SCLN,
-		         KC_A,       KC_R,   LALT_T(KC_S),   LGUI_T(KC_T),       KC_G,                                               KC_M,  RGUI_T(KC_N),  RALT_T(KC_E),     KC_I,      KC_O, 
-		      KC_QUOT,       KC_X,           KC_C,           KC_D,       KC_V,          KC_Z,             KC_BSLS,           KC_K,          KC_H,       KC_COMM,   KC_DOT,   KC_SLSH, 
-    	     _______,     _______,        _______,        _______,    _______,       _______,             _______,        _______,       _______,       KC_MINS,  _______,   _______
-		),
-	[_NUM] = LAYOUT(
-		     KC_PSCR,       KC_F9,         KC_F10,         KC_F11,     KC_F12,                                            KC_MINS,       KC_P7,     KC_P8,     KC_P9,   KC_PPLS, 
-		     KC_SLCK,       KC_F5,          KC_F6,          KC_F7,      KC_F8,                                             KC_EQL,       KC_P4,     KC_P5,     KC_P6,   KC_PSLS, 
-		     KC_PAUS,       KC_F1,          KC_F2,          KC_F3,      KC_F4,        KC_F13,            TO(_KYB),        KC_COMM,       KC_P1,     KC_P2,     KC_P3,   KC_PAST, 
-		     _______,     _______,        _______,        _______,    _______,       _______,             _______,        _______,    TG(_NUM),     KC_P0,   KC_PDOT,   RCTL_T(KC_PENT)
-		),
-	[_NAV] = LAYOUT(
-		     KC_MUTE,     KC_WH_L,        KC_MS_U,         KC_WH_R,       KC_Q,                                           KC_PGUP,     KC_HOME,     KC_UP,    KC_END,   KC_WBAK,  
-		     KC_VOLU,     KC_MS_L,        KC_MS_D,         KC_MS_R,     KC_DEL,                                           KC_PGDN,     KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_WFWD,
-		     KC_VOLD,     KC_WH_U,        KC_BTN1,         KC_WH_D,    KC_BTN2,       KC_MPRV,              KC_MNXT,      KC_MPLY,     KC_ACL0,   KC_ACL1,   KC_ACL2,   KC_BTN3,  
-		    _______,      _______,        _______,         _______,    _______,       _______,              _______,      _______,    TG(_NAV),  _______,    _______,  _______
-		), 		
-	[_SYM] = LAYOUT(
-		     KC_EXLM,       KC_AT,        KC_HASH,          KC_DLR,    KC_PERC,                                            KC_CIRC,     KC_AMPR,    KC_ASTR,   KC_PIPE,   KC_COLN, 
-		     KC_AGIN,     KC_HOME,         KC_END,          KC_INS,     KC_DEL,                                       LGUI(KC_SPC),     KC_LPRN,    KC_LCBR,   KC_LBRC,   KC_PGUP, 
-	         KC_UNDO,      KC_CUT,        KC_COPY,         KC_PSTE,    KC_FIND,        KC_APP,              KC_CAPS,       KC_SLEP,     KC_RPRN,    KC_RCBR,   KC_RBRC,   KC_PGDN, 
-		     _______,     _______,        _______,         _______,    _______,       _______,              _______,       _______,    TG(_SYM),  _______,   KC_TILD,   _______
-		),
-	[_KYB] = LAYOUT(
-		       KC_NO,       KC_NO,     KC_NO,      KC_NO,      KC_NO,                                              KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO, 
-		       KC_NO,       KC_NO,     KC_NO,      KC_NO,      KC_NO,                                              KC_NO, TO(_ISRTNEAO), TO(_MINIMAK),  TO(_QWERTY), TO(_COLEMAK),   
-		       KC_NO,       KC_NO,     KC_NO,      KC_NO,      KC_NO,          KC_NO,                KC_NO,        KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO, 
-		     QK_BOOT,       KC_NO,     KC_NO,      KC_NO,      KC_NO,          KC_NO,                KC_NO,        KC_NO,         KC_NO,        KC_NO,        KC_NO,        KC_NO
-		)
+	[_EXTEND] = LAYOUT(
+			   KC_ESC,       KC_WBAK,       KC_FIND,       KC_WFWD,   KC_INS,                              KC_PGUP,  KC_HOME,     KC_UP,    KC_END,  KC_CAPS,
+		OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL),  KC_RALT,                              KC_PGDN,  KC_LEFT,   KC_DOWN,   KC_RGHT,  KC_DEL,
+			  KC_UNDO,        KC_CUT,       KC_COPY,       KC_LGUI,  KC_PSTE,    KC_NO,           KC_PSCR, KC_RGUI/*?*/,  KC_BSPC,    KC_TAB,    KC_APP,  KC_LOCK,  // KC_LOCK useful for locking SHIFT to select stuff
+		      _______,       _______,   TG(_EXTEND),       _______,  _______,  _______,          _______,    _______,   _______,   _______,   _______, _______
+	),
+	[_FUNCTION] = LAYOUT(
+			  KC_MUTE,       KC_VOLD,       KC_VOLU,       KC_MPLY,  KC_BRIU,                               KC_F12,    KC_F7,     KC_F8,    KC_F9,  C(KC_PLUS),
+		OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL),  KC_BRID,                               KC_F11,    KC_F4,     KC_F5,    KC_F6,  C(KC_MINS),
+			  KC_SLCK,       KC_PAUS,       KC_MPRV,       KC_MNXT,  KC_STOP,     KC_NO,           KC_NO,   KC_F10,    KC_F1,     KC_F2,    KC_F3,  C(KC_0),
+		      _______,       _______, TG(_FUNCTION),       _______,  _______,  _______,          _______,  _______,   _______,     _______,   _______, _______
+	),
+	[_SYMNUM] = LAYOUT(
+			   KC_EXLM,        KC_AT,       KC_HASH,       KC_COLN,  KC_SCLN,                                 KC_PEQL,    KC_P7,      KC_P8,    KC_P9,  KC_PPLS,
+			   KC_BSLS,      KC_PIPE,       KC_LPRN,       KC_LCBR,  KC_LBRC,                                 KC_PAST,    KC_P4,      KC_P5,    KC_P6,  KC_PMNS,
+			   KC_MINS,      KC_UNDS,       KC_RPRN,       KC_RCBR,  KC_RBRC,    KC_NO ,      TLL(_MOUSE),    KC_PCMM,    KC_P1,      KC_P2,    KC_P3,  KC_PSLS,
+		      _______,       _______,   TG(_SYMNUM),       _______,  _______,   _______,          _______,    _______,  _______,   KC_P0,  KC_PDOT,  RCTL_T(KC_PENT)
+	),
+	[_MOUSE] = LAYOUT(
+			   KC_ACL0,      KC_WH_L,       KC_MS_U,       KC_WH_R,  KC_WH_U,                               UC(0X2260),    KC_AMPR,      KC_GRV,    KC_TILD,  UC(0X00D7),
+			   KC_ACL1,      KC_MS_L,       KC_MS_D,       KC_MS_R,  KC_WH_D,                               UC(0X0087),     KC_DLR,     KC_PERC,    KC_CIRC,  UC(0X0081),
+			   KC_ACL2,      KC_BTN4,       KC_BTN3,       KC_BTN2,  KC_BTN1,   KC_BTN5,            KC_NO,  UC(0X0080),    KC_EXLM,       KC_AT,    KC_HASH,  UC(0X00F7),
+		      _______,       _______,    TG(_MOUSE),       _______,  _______,   _______,          _______,     _______,    _______,     _______,    _______,     _______
+	),
+	
 };
 
 
