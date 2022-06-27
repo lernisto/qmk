6b4f095b61f08545564884cfc8fc444b15961ec8
Terrel Shumway <skywalkereternal+carnd@gmail.com>
2022-06-27 01:38:02 -0600

    q>y>x>

--- keyboards/keyboardio/atreus/keymaps/lernisto-minimak/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/lernisto-minimak/keymap.c
@@ -24,16 +24,16 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT(
-		         KC_Q,            KC_C,            KC_L,           KC_M,       KC_K,                                               KC_B,           KC_F,          KC_U,  KC_COMM,   KC_SCLN,
+		         KC_X,            KC_C,            KC_L,           KC_M,       KC_K,                                               KC_J,           KC_F,          KC_U,  KC_COMM,   KC_SCLN,
 		         KC_I,            KC_S,    LALT_T(KC_R),   LGUI_T(KC_T),       KC_G,                                               KC_P,   RGUI_T(KC_N),  RALT_T(KC_E),   KC_A,      KC_O, 
-		         KC_Y,            KC_V,            KC_W,           KC_D,       KC_X,          KC_Z,             KC_BSLS,           KC_J,           KC_H,       KC_MINS,   KC_DOT,   KC_QUOT, 
+		         KC_Q,            KC_V,            KC_W,           KC_D,       KC_Y,          KC_Z,             KC_BSLS,           KC_B,           KC_H,       KC_MINS,   KC_DOT,   KC_QUOT, 
        LCTL_T(KC_ESC),  TLL(_FUNCTION),           KC_NO,   TLL(_EXTEND), OSM(MOD_LSFT),    KC_BSPC,              KC_TAB,  MEH_T(KC_SPC),   TLL(_SYMNUM),       KC_SLSH,   KC_GRV,   RCTL_T(KC_ENT)
 		),
 	[_EXTEND] = LAYOUT(
 			   KC_ESC,       KC_WBAK,       KC_FIND,       KC_WFWD,   KC_INS,                                KC_PGUP,  KC_HOME,     KC_UP,    KC_END,  KC_CAPS,
 		OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL),  KC_RALT,                                KC_PGDN,  KC_LEFT,   KC_DOWN,   KC_RGHT,  KC_DEL,
 			  KC_UNDO,        KC_CUT,       KC_COPY,       KC_PSTE,  KC_LGUI,    KC_NO,        KC_PSCR, /*?*/KC_RGUI,  KC_BSPC,    KC_TAB,    KC_APP,  KC_LOCK,  // KC_LOCK useful for locking SHIFT to select stuff
-		      _______,       _______,   TG(_EXTEND),       _______,  _______,  _______,          _______,    _______,   _______,  _______,   _______, _______
+		      _______,       _______,   TG(_EXTEND),       _______,  _______,  _______,          _______,    _______,   _______,  KC_LSFT,   _______, _______
 	),
 	[_FUNCTION] = LAYOUT(
 			  KC_MUTE,       KC_VOLD,       KC_VOLU,       KC_MPLY,  KC_BRIU,                               KC_F12,    KC_F7,     KC_F8,    KC_F9,  C(KC_PLUS),
